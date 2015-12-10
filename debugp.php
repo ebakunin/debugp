c903029ce6586b613fcad723523fab3314e87218
Eric Christenson <ebakunin@gmail.com>
2015-12-10 10:56:39 -0700

    Minor display improvements
    Added datetime in 'Y-m-d H:i:s' format to the display as well as ini_set() options. Updated related comments.

--- debugp.php
+++ debugp.php
@@ -1,31 +1,40 @@
 <?php
+
+//ini_set('error_reporting', E_ALL);
+//ini_set('error_reporting', E_ERROR | E_WARNING);
+ini_set('error_reporting', E_ERROR);
+ini_set('display_errors', true);
+
 /**
- * Wrapper for handling debug information
+ * Wrapper for displaying debug information
  *
  * @author Eric Christenson (EricChristenson.com)
  * @copyright 2015
- * @version 1.0
+ * @version 1.5
  * @license MIT Public License (http://opensource.org/licenses/MIT)
  *
- * @param mixed $content
- * @param bool $stop (defaults to true)
- * @param bool $format (defaults to true)
+ * @param mixed $content The code to be debugged.
+ * @param bool $stop Whether to exit() the code. Defaults to true.
+ * @param bool $format Whether to use the var_dump() format. Defaults to true.
  */
 if (!function_exists('debugp')) {
-	function debugp($content = array(), $stop = true, $format = true) {
-		$backtrace = current(debug_backtrace());
-		echo '<pre>', $backtrace['file'], ', line ', $backtrace['line'], ':</pre>';
-		
-		if ($format) {
-            var_dump($content);
-		} else {
-			echo "<pre>\n";
-			print_r($content);
-			echo "</pre>\n";
-		}
+    function debugp($content = array(), $stop = true, $format = true) {
+        $backtrace = current(debug_backtrace());
+        echo '<pre>',
+             $backtrace['file'], ",\n",
+             'Line ', $backtrace['line'], ' ',
+             'at ', date('Y-m-d H:i:s'),
+             '</pre>';
 
-		if ($stop) {
-			exit;
-		}
-	}	
+        if ($format) {
+            var_dump($content);
+        } else {
+            echo "<pre>\n";
+            print_r($content);
+            echo "</pre>\n";
+        }
+        if ($stop) {
+            exit;
+        }
+    }
 }
